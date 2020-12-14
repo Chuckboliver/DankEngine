573988bc545347186ca4546a284aa992908a188c
chuckboliver <patpumhak@gmail.com>
2020-12-14 23:19:04 +0700

    file for test serial RX

---
+++ Serial/Serial.ino
@@ -0,0 +1,44 @@
+uint16_t move_to_bytes(String move_uci) {
+  uint16_t data = 4 << 13;
+  for (size_t i = 0 ; i < 2 ; ++i) {
+    data |= map(move_uci[2 * i], 97, 104, 0, 7) << (10 - 6 * i);
+    data |= map(move_uci[2 * i + 1], 49, 56, 0, 7) << (7 - 6 * i);
+  }
+  return data;
+}
+
+void TX(uint16_t data) {
+  if (data >> 8 == 0) {
+    Serial.write(data);
+    delay(10);
+  }
+  else {
+    Serial.write((data >> 8) & 0xFF);
+    Serial.write(data & 0xFF);
+    delay(10);
+  }
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  TX(B00010000);//new game
+  TX(B01101100);//bot level 4
+  TX(B01101000); //bot level 3
+  TX(B00100000); //choose WHITE
+  TX(B00010000);//new game
+  //TX(0x90C6);//e2e4
+  //TX(0x9348);//e7e5
+  //TX(0x9854);//g1f3
+  TX(move_to_bytes("e2e4"));
+  TX(move_to_bytes("e7e5"));
+  TX(move_to_bytes("g1f3"));
+  TX(move_to_bytes("g8f6"));
+  TX(move_to_bytes("f1c4"));
+  TX(move_to_bytes("d7d5"));
+  TX(move_to_bytes("e4d5"));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
